5b9c1f620b849a07eac95dc25ab639fdcefc979b
satyam pawar <satyampawar007@gmail.com>
2025-03-20 16:38:52 +0530

    Add test case for single number return

--- test/models/calculator_test.rb
+++ test/models/calculator_test.rb
@@ -4,4 +4,8 @@ class CalculatorTest < ActiveSupport::TestCase
   test "empty string returns 0" do
     assert_equal 0, Calculator.add("")
   end
+
+  test "single number returns the number" do
+    assert_equal 1, Calculator.add("1")
+  end
 end
