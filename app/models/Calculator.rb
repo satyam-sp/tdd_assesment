4d6dc9866d64a7d5f883e9f5fff6191215b890f6
satyam pawar <satyampawar007@gmail.com>
2025-03-20 16:53:17 +0530

    Implement custom delimiter handler

--- app/models/Calculator.rb
+++ app/models/Calculator.rb
@@ -1,7 +1,20 @@
 class Calculator
-    def self.add(numbers)
-        return 0 if numbers.empty?
-        numbers.split(/[\n,]/).map(&:to_i).sum
-    end
+	def self.add(numbers)
+		return 0 if numbers.empty?
+		if numbers.start_with?("//")
+			delimiter, numbers = extract_delimiter(numbers)
+		else
+			delimiter = /[\n,]/
+		end
+		numbers.split(delimiter).map(&:to_i).sum
+	end
+
+	private
+
+	def self.extract_delimiter(numbers)
+		first_line, rest_of_numbers = numbers.split("\n", 2)
+		delimiter = first_line[2..-1]
+		[delimiter, rest_of_numbers]
+	end
 end
   
